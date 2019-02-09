851ead762b3f971936ca4c2803ea1bbd6eef13ab
LusinehTsarukyan <tsarukyanlusine@gmail.com>
2019-02-09 17:01:47 +0400

    adding interfaces

---
+++ src/main/java/classes/interfaces/ITicketFactory.java
@@ -0,0 +1,8 @@
+package classes.interfaces;
+
+public interface ITicketFactory {
+   void create();
+   void open();
+   void update();
+   void cancel();
+}
---
+++ src/main/java/classes/interfaces/IUserFactory.java
@@ -0,0 +1,5 @@
+package classes.interfaces;
+
+public interface IUserFactory {
+   void create();
+}
