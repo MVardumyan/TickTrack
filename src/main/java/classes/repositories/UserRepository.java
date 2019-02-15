d9c308ad96887625c6fd2d2ffb3d857291406ded
LusinehTsarukyan <37470797+LusinehTsarukyan@users.noreply.github.com>
2019-02-15 17:08:51 +0400

    User manager and ticket manager (#10)
    * addComment() done, need to be continued
    
    * protobuf changes, in progress classes(usermngr,ticketmngr)

--- src/main/java/classes/repositories/UserRepository.java
+++ src/main/java/classes/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package classes.repositories;
 
 import classes.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, String> {
+   Optional<User> findByName(String name);
 }
