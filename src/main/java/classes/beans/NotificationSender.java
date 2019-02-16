fa2669cfb3f5e504e63143a4360d016eb8b699f1
David Avetisyan <davetisyan114@gmail.com>
2019-02-16 11:17:10 +0400

    NotificationSender

--- src/main/java/classes/beans/NotificationSender.java
+++ src/main/java/classes/beans/NotificationSender.java
@@ -1,5 +1,21 @@
 package classes.beans;
 
+import net.sargue.mailgun.Configuration;
+import net.sargue.mailgun.Mail;
+
 public class NotificationSender {
-   public static boolean sendMail(String address, String text){return false;}
+
+   public static boolean sendMail(String address, String text){
+       Configuration configuration = new Configuration()
+               .domain("sandbox640825bd39064eb58cc7d62b2de57f84.mailgun.org")
+               .apiKey("cf1bb82f5ef89c6a8aba2a56c057d931-1b65790d-9b99b4f0")
+               .from("TickTrack", "TickTrack@gmail.com");
+
+       return Mail.using(configuration)
+               .to(address)
+               .subject("TickTrack notification")
+               .text(text)
+               .build()
+               .send().isOk();
+   }
 }
