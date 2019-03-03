9e66f59a28165d1636355a8eef9dfe947c5b951d
MVardumyan <mikayel2505@gmail.com>
2019-03-03 21:59:03 +0400

    Login validation in backend (#25)
    * login validation at backend added
    
    * added test
    
    * minor addition in test

--- backend/src/main/java/ticktrack/util/PasswordHandler.java
+++ backend/src/main/java/ticktrack/util/PasswordHandler.java
@@ -9,37 +9,36 @@ import static com.kosprov.jargon2.api.Jargon2.jargon2Verifier;
 
 public class PasswordHandler {
 
-   static final private Logger logger = LoggerFactory.getLogger(PasswordHandler.class);
-   static private final Jargon2.Hasher hasher = jargon2Hasher()
-           .type(Jargon2.Type.ARGON2d)
-           .memoryCost(65536)
-           .timeCost(3)
-           .parallelism(4)
-           .hashLength(16);
-   static private final Jargon2.Verifier verifier  = jargon2Verifier();
+    private static final Logger logger = LoggerFactory.getLogger(PasswordHandler.class);
+    private static final Jargon2.Hasher hasher = jargon2Hasher()
+            .type(Jargon2.Type.ARGON2d)
+            .memoryCost(65536)
+            .timeCost(3)
+            .parallelism(4)
+            .hashLength(16);
+    private static final Jargon2.Verifier verifier = jargon2Verifier();
 
 
-   static public String encode(String password){
+    public static String encode(String password) {
 
-       if(password != null && password.equals("")){
+        if (password == null || password.equals("")) {
+            logger.debug("something went wrong : password = " + password);
+            return null;
+        } else {
             byte[] passwordInBytes = password.getBytes();
             return hasher.password(passwordInBytes).encodedHash();
-       } else {
-           logger.debug("something went wrong : password = " + password);
-           return null;
-       }
+        }
 
-   }
+    }
 
+    public static boolean verifyPassword(String password, String encodedHash) throws IllegalArgumentException {
+        if (password == null || encodedHash == null || password.equals("") || encodedHash.equals("")) {
+            throw new IllegalArgumentException("password or encoded Hash == null or == '' " + " " +
+                    "==> password :" + password + "  encodedHash :" + encodedHash);
+        }
 
-   public static boolean verifyPassword(String password, String encodedHash) throws IllegalArgumentException {
-       if(password == null || encodedHash == null || password.equals("") || encodedHash.equals("")){
-           throw new IllegalArgumentException("password or encodeedHash == null or == '' " + " " +
-                   "==> password :" + password + "  encodedHash :" + encodedHash );
-       }
-
-         byte[] passwordInBytes = password.getBytes();
-         return verifier.hash(encodedHash).password(passwordInBytes).verifyEncoded();
-   }
+        byte[] passwordInBytes = password.getBytes();
+        return verifier.hash(encodedHash).password(passwordInBytes).verifyEncoded();
+    }
 
 }
