5233786f8c3566751b0d7e59e107062dca271d0c
MVardumyan <mikayel2505@gmail.com>
2019-03-05 17:14:54 +0400

    Jsp template for ticket search (#28)
    * in progress
    
    * jsp problem resolved
    
    * some fields added
    
    * minor
    
    * autocomplete functionality added to search page
    
    * CustomJsonParser moved to common; group added to search page

--- frontend/src/main/java/ticktrack/frontend/controller/SearchController.java
+++ frontend/src/main/java/ticktrack/frontend/controller/SearchController.java
@@ -1,45 +1,100 @@
 package ticktrack.frontend.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.util.JsonFormat;
+import common.helpers.CustomJsonParser;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import ticktrack.proto.Msg;
 
 import java.io.IOException;
+import java.util.List;
+
+import static common.helpers.CustomJsonParser.*;
 
 @Controller
 public class SearchController {
     private final OkHttpClient httpClient;
+    private final Logger logger = LoggerFactory.getLogger(SearchController.class);
 
     @Autowired
     public SearchController(OkHttpClient httpClient) {
         this.httpClient = httpClient;
     }
 
-    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @RequestMapping(value = "search", method = RequestMethod.GET)
     public String displaySearchPage(ModelMap model) {
-        Request request = new Request.Builder()
-                .url("http://localhost:9001/backend/v1/categories/getAll")
+        Request categoriesRequest = new Request.Builder()
+                .url("http://localhost:9001/backend/v1/categories/getAllActive")
+                .build();
+        Request groupsRequest = new Request.Builder()
+                .url("http://localhost:9001/backend/v1/userGroups/getAll")
                 .build();
 
-        try (Response response = httpClient.newCall(request).execute()) {
-            Msg.Builder builder = Msg.newBuilder();
-            JsonFormat.parser().merge(response.body().string(), builder);
-            Msg result = builder.build();
+        try (Response categoryResponse = httpClient.newCall(categoriesRequest).execute();
+        Response groupResponse = httpClient.newCall(groupsRequest).execute()) {
+            if(categoryResponse.code()==200) {
+                Msg result = jsonToProtobuf(categoryResponse.body().string());
 
-            model.put("categoryList", result.getCategoryOperation().getCategoryOpGetAllResponse().getCategoryNameList());
+                model.put("categoryList", result.getCategoryOperation().getCategoryOpGetAllResponse().getCategoryNameList());
+            } else {
+                logger.warn("Error received from backend, unable to get categories list: {}", categoryResponse.message());
+            }
 
+            if(groupResponse.code() == 200) {
+                Msg result = jsonToProtobuf(groupResponse.body().string());
+
+                model.put("groupList", result.getUserGroupOperation().getUserGroupOpGetAllResponse().getGroupNameList());
+            } else {
+                logger.warn("Error received from backend, unable to get group list: {}", groupResponse.message());
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Internal error, unable to get categories list", e);
         }
 
         return "searchTicket";
+    }
+
+    @RequestMapping(value = "searchTickets", method = RequestMethod.GET)
+    @ResponseBody
+    List<Msg.TicketInfo> searchTickets(@RequestBody String jsonRequest) {
+
+        System.out.println("JSON RESULT:\n" + jsonRequest);
+
+        Request usersRequest = new Request.Builder()
+                .url("http://localhost:9001/backend/v1/searchUsers/")
+                .build();
+
+        return null;
+    }
+
+    @RequestMapping(value = "searchUsers", method = RequestMethod.GET)
+    @ResponseBody
+    List<String> searchUsers(@RequestParam("term") String term) {
+        Request usersRequest = new Request.Builder()
+                .url("http://localhost:9001/backend/v1/searchUsers/" + term)
+                .build();
+
+        try (Response response = httpClient.newCall(usersRequest).execute()) {
+            if(response.code()==200) {
+                ObjectMapper mapper = new ObjectMapper();
+
+                return mapper.readValue(response.body().string(), mapper.getTypeFactory().constructCollectionType(List.class, String.class));
+            } else {
+                logger.warn("Error received from backend, unable to get users list: {}", response.message());
+            }
+        } catch (IOException e) {
+            logger.error("Internal error, unable to get users list", e);
+        }
+        return null;
     }
 
 }
