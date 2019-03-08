4c4cf8175d45252ca828367b52deb90f7bd87a86
LusinehTsarukyan <tsarukyanlusine@gmail.com>
2019-03-08 15:21:57 +0400

    ticket info by id variable

--- frontend/src/main/java/ticktrack/frontend/controller/PersonalInfoController.java
+++ frontend/src/main/java/ticktrack/frontend/controller/PersonalInfoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ticktrack.proto.Msg;
 
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Controller
@@ -23,9 +24,9 @@ public class PersonalInfoController {
     }
 
     @RequestMapping(value = "/personalInfo", method = RequestMethod.GET)
-    public String displayPersonalInfoPage(ModelMap model) {
+    public String displayPersonalInfoPage(ModelMap model, HttpSession httpSession) {
         Request request = new Request.Builder()
-                .url("http://localhost:9001/backend/v1/users/getUser/Lusine")
+                .url("http://localhost:9001/backend/v1/users/getUser/" + httpSession.getAttribute("name"))
                 .build();
         try (Response response = httpClient.newCall(request).execute()) {
             Msg.Builder builder = Msg.newBuilder();
