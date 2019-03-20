1ca31420396e61679f6a44341653bd681d90b4db
Mikayel Vardumyan <mikayel2505@gmail.com>
2019-03-20 12:25:52 +0400

    periodic task for deadline expiration check done

--- backend/src/main/java/ticktrack/Backend.java
+++ backend/src/main/java/ticktrack/Backend.java
@@ -1,13 +1,55 @@
 package ticktrack;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import ticktrack.tasks.CheckDeadlineTask;
 
 @SpringBootApplication
 public class Backend {
 
-   public static void main(String[] args) {
-      SpringApplication.run(Backend.class, args);
-   }
+    @Bean
+    @Scope("singleton")
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler
+                = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(2);
+        threadPoolTaskScheduler.setThreadNamePrefix(
+                "RegularTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Backend.class, args);
+
+        configureScheduledTasks(context);
+    }
+
+    /**
+     * method starts periodic tasks for:
+     * 1) deadline expiration notification (with start time - 24:00 and period - 1 day);
+     * 2) password change link expiration (in case of system restart)
+     * @param context Spring application context needed to get scheduler and task beans
+     */
+    private static void configureScheduledTasks(ConfigurableApplicationContext context) {
+        ThreadPoolTaskScheduler scheduler = context.getBean(ThreadPoolTaskScheduler.class);
+
+        long period = 1000*60*60*24L; //1 day in milliseconds
+
+        DateTime currentTime = DateTime.now().withZone(DateTimeZone.forID("Asia/Yerevan"));
+        DateTime startTime = new DateTime(currentTime)
+                .plusDays(1)
+                .withHourOfDay(0)
+                .withMinuteOfHour(0)
+                .withSecondOfMinute(0)
+                .withZone(DateTimeZone.forID("Asia/Yerevan"));
+
+        scheduler.scheduleAtFixedRate(context.getBean(CheckDeadlineTask.class), startTime.toDate(), period);
+    }
 
 }
--- backend/src/main/java/ticktrack/repositories/TicketRepository.java
+++ backend/src/main/java/ticktrack/repositories/TicketRepository.java
@@ -4,6 +4,5 @@ import ticktrack.entities.Ticket;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TicketRepository extends CrudRepository<Ticket, Long> {
-   //Optional<Ticket> findByID(long ID);
-
+    Iterable<Ticket> findAllByDeadlineNotNull();
 }
