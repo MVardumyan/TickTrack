b2b134ec3a46fbed08b7c0f0dc74e1c3ccce4bc2
Mikayel Vardumyan <mikayel2505@gmail.com>
2019-03-25 10:20:34 +0400

    unit tests for CategoryManager, SearchManager, UserGroupManager improved

--- backend/src/test/java/ticktrack/managers/CategoryManagerTest.java
+++ backend/src/test/java/ticktrack/managers/CategoryManagerTest.java
@@ -1,5 +1,7 @@
 package ticktrack.managers;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import ticktrack.entities.Category;
 import ticktrack.interfaces.ICategoryManager;
 import org.junit.jupiter.api.Test;
@@ -7,61 +9,112 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ticktrack.proto.Msg;
 import ticktrack.repositories.CategoryRepository;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static ticktrack.proto.Msg.CategoryOp;
+import static ticktrack.proto.Msg.CommonResponse.ResponseType.Failure;
+import static ticktrack.proto.Msg.CommonResponse.ResponseType.Success;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CategoryManagerTest {
 
    @Autowired
-   private ICategoryManager categoryManager;
+   private CategoryManager categoryManager;
    @Autowired
    private CategoryRepository categoryRepository;
 
-   @Test
-   void createAndDeactivateCategory() {
-      categoryManager.createCategory("testCategory1");
+   @BeforeEach
+   void initTestData() {
+      Category category1 = new Category();
+      Category category2 = new Category();
+      category1.setName("testCategory1");
+      category2.setName("testCategory2");
 
-      Category category = categoryRepository.findByName("testCategory1").get();
-      assertNotNull(category);
+      categoryRepository.save(category1);
+      categoryRepository.save(category2);
+   }
+
+   @Test
+   void createCategory() {
+      Msg.CommonResponse commonResponse = categoryManager.createCategory("testCategory3");
+
+      assertEquals(Success, commonResponse.getResponseType());
+
+      Optional<Category> testCategory = categoryRepository.findByName("testCategory1");
+
+      assertTrue(testCategory.isPresent());
+      assertEquals("testCategory1", testCategory.get().getName());
+   }
+
+   @Test
+   void createCategoryWithExistingName() {
+      Msg.CommonResponse commonResponse = categoryManager.createCategory("testCategory1");
+
+      assertEquals(Failure, commonResponse.getResponseType());
+   }
+
+   @Test
+   void updateCategory() {
+      Msg.CommonResponse commonResponse = categoryManager.changeName(CategoryOp.CategoryOpUpdateRequest.newBuilder()
+              .setOldName("testCategory2")
+              .setNewName("testCategory3")
+              .build());
+
+      assertEquals(Success, commonResponse.getResponseType());
+
+      Optional<Category> testCategory = categoryRepository.findByName("testCategory3");
+      assertTrue(testCategory.isPresent());
+      assertEquals("testCategory3", testCategory.get().getName());
+   }
+
+   @Test
+   void updateWithInvalidOldName() {
+      Msg.CommonResponse commonResponse = categoryManager.changeName(CategoryOp.CategoryOpUpdateRequest.newBuilder()
+              .setOldName("testCategory00")
+              .setNewName("testCategory3")
+              .build());
+
+      assertEquals(Failure, commonResponse.getResponseType());
+   }
+
+   @Test
+   void deactivateCategory() {
+      Msg.CommonResponse commonResponse = categoryManager.deactivateCategory("testCategory1");
+
+      assertEquals(Success, commonResponse.getResponseType());
+
+      Optional<Category> testCategory = categoryRepository.findByName("testCategory1");
+      assertTrue(testCategory.isPresent());
+      assertTrue(testCategory.get().isDeactivated());
+   }
+
+   @Test
+   void getCategory() {
+      Category category = categoryManager.get("testCategory1");
+
       assertEquals("testCategory1", category.getName());
-
-      categoryManager.deactivateCategory("testCategory1");
-
-      category = categoryRepository.findByName("testCategory1").get();
-      assertTrue(category.isDeactivated());
    }
 
    @Test
-   void createAndUpdateCategory() {
-      categoryManager.createCategory("testCategory2");
+   void getInvalidCategory() {
+      Category category = categoryManager.get("testCategory00");
 
-      Category category = categoryRepository.findByName("testCategory2").get();
-      assertNotNull(category);
-      assertEquals("testCategory2", category.getName());
-
-      categoryManager.changeName(CategoryOp.CategoryOpUpdateRequest.newBuilder()
-         .setOldName("testCategory2")
-         .setNewName("testCategory3")
-         .build());
-
-      category = categoryRepository.findByName("testCategory3").get();
-      assertNotNull(category);
-      assertEquals("testCategory3", category.getName());
+      assertNull(category);
    }
 
-//   @Test
-//   void getAll() {
-//      categoryRepository.deleteAll();
-//      Category category1 = new Category("test1");
-//      categoryRepository.save(category1);
-//      Category category2 = new Category("test2");
-//      categoryRepository.save(category2);
-//
-//      CategoryOp.CategoryOpGetAllResponse result = categoryManager.getAll();
-//      assertEquals(2, result.getCategoryNameCount());
-//   }
+   @Test
+   void getAll() {
+      CategoryOp.CategoryOpGetAllResponse result = categoryManager.getAll();
+      assertEquals(2, result.getCategoryInfoCount());
+   }
+
+   @AfterEach
+   void clearTestDate() {
+      categoryRepository.deleteAll();
+   }
 }
--- backend/src/test/java/ticktrack/managers/SearchManagerTest.java
+++ backend/src/test/java/ticktrack/managers/SearchManagerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import ticktrack.entities.Category;
 import ticktrack.entities.Ticket;
 import ticktrack.entities.User;
+import ticktrack.enums.Gender;
 import ticktrack.enums.TicketPriority;
 import ticktrack.enums.TicketStatus;
 import common.enums.UserRole;
@@ -32,36 +33,45 @@ class SearchManagerTest {
    @Autowired
    private CategoryRepository categoryRepository;
    private User testUser;
-   private Ticket testTicket;
    private Category testCategory;
-   private long testOpenDate;
 
    @BeforeEach
    void initTestData() {
       testUser = new User();
       testUser.setUsername("mik");
       testUser.setActiveStatus(true);
-      testUser.setFirstName("mikayel");
-      testUser.setLastName("vardumyan");
-      testUser.setEmail("mikayel2505@gmail.com");
+      testUser.setFirstName("john");
+      testUser.setLastName("doe");
+      testUser.setEmail("someone@gmail.com");
       testUser.setPassword("password");
       testUser.setRole(UserRole.BusinessUser);
+      testUser.setGender(Gender.Male);
+      testUser.setRegistrationTime(new Timestamp(System.currentTimeMillis()));
       userRepository.save(testUser);
 
       testCategory = new Category();
       testCategory.setName("Category00");
       categoryRepository.save(testCategory);
 
-      testTicket = new Ticket();
+      Ticket testTicket = new Ticket();
       testTicket.setSummary("this is test");
       testTicket.setDescription("test");
       testTicket.setStatus(TicketStatus.Open);
       testTicket.setPriority(TicketPriority.Medium);
       testTicket.setCreator(testUser);
       testTicket.setCategory(testCategory);
-      testOpenDate = System.currentTimeMillis();
-      testTicket.setOpenDate(new Timestamp(testOpenDate));
+      testTicket.setOpenDate(new Timestamp(System.currentTimeMillis()));
       ticketRepository.save(testTicket);
+
+      Ticket testTicket2 = new Ticket();
+      testTicket2.setSummary("this is test");
+      testTicket2.setDescription("test");
+      testTicket2.setStatus(TicketStatus.Open);
+      testTicket2.setPriority(TicketPriority.Low);
+      testTicket2.setCreator(testUser);
+      testTicket2.setCategory(testCategory);
+      testTicket2.setOpenDate(new Timestamp(System.currentTimeMillis()));
+      ticketRepository.save(testTicket2);
    }
 
    @Test
@@ -75,15 +85,25 @@ class SearchManagerTest {
       assertEquals("mik", response.getTicketInfo(0).getCreator());
    }
 
-//   @Test
-//   void searchByOpenDate() {
-//      Msg.SearchOp.SearchOpRequest request = Msg.SearchOp.SearchOpRequest.newBuilder()
-//         .setOpenDateStart(testOpenDate)
-//         .build();
-//
-//      Msg.SearchOp.SearchOpResponse response = searchManager.searchByCriteria(request);
-//      assertEquals("mik", response.getTicketInfo(0).getCreator());
-//   }
+   @Test
+   void searchByCategory() {
+      Msg.SearchOp.SearchOpRequest request = Msg.SearchOp.SearchOpRequest.newBuilder()
+              .addCategory("Category00")
+              .build();
+
+      Msg.SearchOp.SearchOpResponse response = searchManager.searchByCriteria(request,1,10);
+      assertEquals(2, response.getTicketInfoCount());
+   }
+
+   @Test
+   void searchByCreator() {
+      Msg.SearchOp.SearchOpRequest request = Msg.SearchOp.SearchOpRequest.newBuilder()
+              .setCreator("mik")
+              .build();
+
+      Msg.SearchOp.SearchOpResponse response = searchManager.searchByCriteria(request,1,10);
+      assertEquals(2, response.getTicketInfoCount());
+   }
 
    @Test
    void searchByTwoCriterias() {
@@ -108,7 +128,7 @@ class SearchManagerTest {
 
    @AfterEach
    void clearTestData() {
-      ticketRepository.delete(testTicket);
+      ticketRepository.deleteAll();
       categoryRepository.delete(testCategory);
       userRepository.delete(testUser);
    }
