91baa9e7f1ddeb6211070134382d9c0a4244d713
Mikayel Vardumyan <mikayel2505@gmail.com>
2019-03-25 12:39:50 +0400

    UserManager tests

--- backend/src/test/java/ticktrack/managers/UserManagerTest.java
+++ backend/src/test/java/ticktrack/managers/UserManagerTest.java
@@ -13,11 +13,14 @@ import ticktrack.enums.Gender;
 import common.enums.UserRole;
 import ticktrack.proto.Msg;
 import ticktrack.repositories.GroupRepository;
+import ticktrack.repositories.PasswordLinkRepository;
 import ticktrack.repositories.UserRepository;
 import common.helpers.PasswordHandler;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ticktrack.proto.Msg.CommonResponse.ResponseType.Failure;
+import static ticktrack.proto.Msg.CommonResponse.ResponseType.Success;
 
 import java.sql.Timestamp;
 import java.util.Optional;
@@ -31,44 +34,15 @@ class UserManagerTest {
     GroupRepository groupRepository;
     @Autowired
     UserRepository userRepository;
-    private UserGroup testGroup;
+    @Autowired
+    private PasswordLinkRepository passwordLinkRepository;
 
     @BeforeEach
     void initTestData() {
-        testGroup = new UserGroup();
+        UserGroup testGroup = new UserGroup();
         testGroup.setName("testGroup");
         groupRepository.save(testGroup);
-    }
 
-    @Test
-    void createUser() {
-        Msg.UserOp.UserOpCreateRequest request = Msg.UserOp.UserOpCreateRequest.newBuilder()
-                .setUsername("user1")
-                .setFirstname("John")
-                .setLastname("Doe")
-                .setEmail("something@somemail.com")
-                .setPassword("password")
-                .setRole(Msg.UserRole.BusinessUser)
-                .setGender(Msg.UserOp.Gender.Male)
-                .build();
-
-        userManager.create(request);
-
-        Optional<User> result = userRepository.findByUsername("user1");
-        assertTrue(result.isPresent());
-        User user = result.get();
-
-        assertTrue(user.isActive());
-        assertEquals("John", user.getFirstName());
-        assertEquals("Doe", user.getLastName());
-        assertEquals("something@somemail.com", user.getEmail());
-        assertEquals(UserRole.BusinessUser, user.getRole());
-
-        userRepository.delete(user);
-    }
-
-    @Test
-    void SearchByRole() {
         User user1 = new User();
         user1.setUsername("user1");
         user1.setFirstName("Jane");
@@ -77,6 +51,7 @@ class UserManagerTest {
         user1.setPassword("password");
         user1.setGender(Gender.Female);
         user1.setRole(UserRole.RegularUser);
+        user1.setActiveStatus(true);
         user1.setRegistrationTime(new Timestamp(System.currentTimeMillis()));
 
         userRepository.save(user1);
@@ -89,10 +64,147 @@ class UserManagerTest {
         user2.setGender(Gender.Male);
         user2.setPassword("password");
         user2.setRole(UserRole.RegularUser);
+        user2.setActiveStatus(true);
         user2.setRegistrationTime(new Timestamp(System.currentTimeMillis()));
 
         userRepository.save(user2);
+    }
 
+    @Test
+    void createUser() {
+        Msg.UserOp.UserOpCreateRequest request = Msg.UserOp.UserOpCreateRequest.newBuilder()
+                .setUsername("user3")
+                .setFirstname("John")
+                .setLastname("Smith")
+                .setEmail("someone@somemail.com")
+                .setPassword("password")
+                .setGender(Msg.UserOp.Gender.Male)
+                .setRole(Msg.UserRole.BusinessUser)
+                .build();
+
+        Msg.CommonResponse commonResponse = userManager.create(request);
+
+        assertEquals(Success, commonResponse.getResponseType());
+
+        Optional<User> result = userRepository.findByUsername("user3");
+        assertTrue(result.isPresent());
+        User user = result.get();
+
+        assertTrue(user.isActive());
+        assertEquals("John", user.getFirstName());
+        assertEquals("Smith", user.getLastName());
+        assertEquals("someone@somemail.com", user.getEmail());
+        assertEquals(UserRole.BusinessUser, user.getRole());
+    }
+
+    @Test
+    void createUserWithExistingUsername() {
+        Msg.UserOp.UserOpCreateRequest request = Msg.UserOp.UserOpCreateRequest.newBuilder()
+                .setUsername("user1")
+                .setFirstname("John")
+                .setLastname("Smith")
+                .setEmail("someone@somemail.com")
+                .setPassword("password")
+                .setGender(Msg.UserOp.Gender.Male)
+                .setRole(Msg.UserRole.BusinessUser)
+                .build();
+
+        Msg.CommonResponse commonResponse = userManager.create(request);
+
+        assertEquals(Failure, commonResponse.getResponseType());
+    }
+
+    @Test
+    void createUserWithGroup() {
+        Msg.UserOp.UserOpCreateRequest request = Msg.UserOp.UserOpCreateRequest.newBuilder()
+                .setUsername("user3")
+                .setFirstname("John")
+                .setLastname("Smith")
+                .setEmail("someone@somemail.com")
+                .setPassword("password")
+                .setGender(Msg.UserOp.Gender.Male)
+                .setRole(Msg.UserRole.RegularUser)
+                .setGroup("testGroup")
+                .build();
+
+        Msg.CommonResponse commonResponse = userManager.create(request);
+
+        assertEquals(Success, commonResponse.getResponseType());
+
+        Optional<User> result = userRepository.findByUsername("user3");
+        assertTrue(result.isPresent());
+        User user = result.get();
+
+        assertTrue(user.isActive());
+        assertEquals(UserRole.RegularUser, user.getRole());
+        assertEquals("testGroup", user.getGroup().getName());
+    }
+
+    @Test
+    void creteUserWithInvalidGroup() {
+        Msg.UserOp.UserOpCreateRequest request = Msg.UserOp.UserOpCreateRequest.newBuilder()
+                .setUsername("user3")
+                .setFirstname("John")
+                .setLastname("Smith")
+                .setEmail("someone@somemail.com")
+                .setPassword("password")
+                .setGender(Msg.UserOp.Gender.Male)
+                .setRole(Msg.UserRole.RegularUser)
+                .setGroup("group000")
+                .build();
+
+        Msg.CommonResponse commonResponse = userManager.create(request);
+
+        assertEquals(Failure, commonResponse.getResponseType());
+    }
+
+    @Test
+    void deactivateUser() {
+        Msg.CommonResponse commonResponse = userManager.deactivate("user1");
+
+        assertEquals(Success, commonResponse.getResponseType());
+
+        Optional<User> result = userRepository.findByUsername("user1");
+        assertTrue(result.isPresent());
+        assertFalse(result.get().isActive());
+    }
+
+    @Test
+    void deactivateInvalidUser() {
+        Msg.CommonResponse commonResponse = userManager.deactivate("user3");
+
+        assertEquals(Failure, commonResponse.getResponseType());
+    }
+
+    @Test
+    void deactivateDeactivatedUser() {
+        Optional<User> result = userRepository.findByUsername("user1");
+        assertTrue(result.isPresent());
+        User user = result.get();
+        user.setActiveStatus(false);
+        userRepository.save(user);
+
+        Msg.CommonResponse commonResponse = userManager.deactivate("user1");
+
+        assertEquals(Failure, commonResponse.getResponseType());
+    }
+
+    @Test
+    void getUser() {
+        Msg.UserOp.UserOpGetResponse response = userManager.get("user1");
+
+        assertEquals(1, response.getUserInfoCount());
+    }
+
+    @Test
+    void getInvalidUser() {
+        Msg.UserOp.UserOpGetResponse response = userManager.get("user000");
+
+        assertEquals(0, response.getUserInfoCount());
+    }
+
+    @Test
+    void GetByRole() {
         Msg.UserOp.UserOpGetByRoleRequest request = Msg.UserOp.UserOpGetByRoleRequest.newBuilder()
                 .setCriteria(Msg.UserOp.UserOpGetByRoleRequest.Criteria.RegularUser)
                 .build();
@@ -102,39 +214,23 @@ class UserManagerTest {
         assertEquals(2, result.getUserInfoCount());
         assertEquals("user1", result.getUserInfo(0).getUsername());
         assertEquals("user2", result.getUserInfo(1).getUsername());
-
-        userRepository.delete(user1);
-        userRepository.delete(user2);
     }
 
     @Test
     void validateLogin() {
-        User user = new User();
-        user.setUsername("user");
-        user.setFirstName("Jane");
-        user.setLastName("Doe");
-        user.setEmail("jane@somemail.com");
-        user.setPassword("password");
-        user.setGender(Gender.Female);
-        user.setRole(UserRole.RegularUser);
-        user.setRegistrationTime(new Timestamp(System.currentTimeMillis()));
-
-        userRepository.save(user);
-
         Msg.LoginRequest request = Msg.LoginRequest.newBuilder()
-                .setUsername("user")
+                .setUsername("user1")
                 .setPassword(PasswordHandler.encode("password"))
                 .build();
 
         Msg.CommonResponse result = userManager.validateLoginInformation(request);
-        assertEquals(Msg.CommonResponse.ResponseType.Success, result.getResponseType());
-
-        userRepository.delete(user);
+        assertEquals(Success, result.getResponseType());
     }
 
     @AfterEach
     void deleteTestData() {
-        groupRepository.delete(testGroup);
+        userRepository.deleteAll();
+        groupRepository.deleteAll();
     }
 
 
